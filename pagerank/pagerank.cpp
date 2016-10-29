3a48ad949a298b13ed1923bf8771d3552708a56b
Tim Weidner <mail@timweidner.com>
2016-10-29 21:37:23 +0200

    local version works correctly now according to wikipedia example

--- pagerank/pagerank.cpp
+++ pagerank/pagerank.cpp
@@ -21,11 +21,9 @@ struct Thread_args {
 struct Chunk {
     int begin;
     int size;
-    std::vector<int> test;
+    std::vector<int> inlinks;
     std::vector<bool> exists;
-    std::vector<bool> dangling;
     std::vector<int> outlinks;
-    std::vector<std::vector<int>> W_index;
     std::vector<double> pagerank;
 };
 
@@ -39,9 +37,8 @@ Chunk* chunk;
 // Pagerank Constants
 int    MAX_VERTICES     = 2000000;
 int    MAX_DEGREE       = 16;
-double INITIAL_PAGERANK = 0.15;
 double DAMPING_FACTOR   = 0.85;
-int    ITERATIONS       = 1;
+int    ITERATIONS       = 40;
 
 
 // ArgoDMS global variables
@@ -52,9 +49,8 @@ bool*        lock_flag;
 double*      dp;
 double*      dp_threads;
 double*      pagerank;
-int*         test;
+int*         inlinks;
 bool*        exists;
-bool*        dangling;
 int*         outlinks;
 int**        W_index;
 
@@ -78,8 +74,9 @@ void* do_work(void* argptr) {
 
         argo::barrier(local_num_threads);
 
+        dp_threads[global_thread_id] = 0;
         for(int i = data_begin; i < data_end; i++)
-            if(chunk->dangling[i])
+            if(chunk->outlinks[i] == 0)
                 dp_threads[global_thread_id] += DAMPING_FACTOR*(pagerank[chunk->begin+i]/vertices);
 
         lock->lock();
@@ -90,12 +87,10 @@ void* do_work(void* argptr) {
 
         for(int i = data_begin; i < data_end; i++) {
             if (chunk->exists[i]) {
-                chunk->pagerank[i] = 0.15;
-                for(int j = 0; j < chunk->test[i]; j++)
-                    chunk->pagerank[i] += DAMPING_FACTOR * (pagerank[chunk->W_index[chunk->begin+i][j]] / outlinks[chunk->W_index[chunk->begin+i][j]]);
+                chunk->pagerank[i] = ((1-DAMPING_FACTOR)/vertices) + (*dp);
+                for(int j = 0; j < chunk->inlinks[i]; j++)
+                    chunk->pagerank[i] += DAMPING_FACTOR * (pagerank[W_index[chunk->begin+i][j]] / outlinks[W_index[chunk->begin+i][j]]);
             }
-            if (chunk->pagerank[i] >= 1.0)
-                chunk->pagerank[i] = 1.0;
         }
 
         argo::barrier(local_num_threads);
@@ -105,6 +100,7 @@ void* do_work(void* argptr) {
                 pagerank[chunk->begin+i] = chunk->pagerank[i];
 
         argo::barrier(local_num_threads);
+
         iterations--;
     }
     return NULL;
@@ -123,9 +119,8 @@ void read_graph_from_file(std::string filename) {
     *io_node_id = argo::node_id();
 
     for(int i = 0; i < MAX_VERTICES; i++) {
-        test[i]     = 0;
+        inlinks[i]  = 0;
         exists[i]   = false;
-        dangling[i] = false;
         outlinks[i] = 0;
         for(int j = 0; j < MAX_DEGREE; j++)
             W_index[i][j] = std::numeric_limits<int>::max();
@@ -136,19 +131,15 @@ void read_graph_from_file(std::string filename) {
 
     std::string line;
     while (input >> number0 >> number1) {
-        inter = test[number1];
+        inter = inlinks[number1];
         W_index[number1][inter] = number0;
-        test[number1]++;
+        inlinks[number1]++;
         outlinks[number0]++;
         exists[number0] = true;
         exists[number1] = true;
-        dangling[number1] = true;
         if(number0 > vertex_cnt) vertex_cnt = number0;
         if(number1 > vertex_cnt) vertex_cnt = number1;
     }
-    for(int i = 0; i < MAX_VERTICES; i++)
-        if(exists[i] && dangling[i])
-            dangling[i] = false;
     *global_size = ++vertex_cnt;
 
     lock->unlock();
@@ -175,8 +166,14 @@ int main(int argc, char* argv[]) {
     // Startup ArgoDSM
     argo::init(0.5*1024*1024*1024UL);
 
+    if (argc > 4) {
+        std::cout << "Wrong number of arguments" << std::endl;
+        return 0;
+    }
+
     global_num_threads = atoi(argv[1]);
-    std::string filename = argv[2];
+    std::string input_filename = argv[2];
+    std::string output_filename = argv[3];
 
     // Declare ArgoDSM global memory variables
     arguments   = argo::conew_array<Thread_args>(global_num_threads);
@@ -188,17 +185,18 @@ int main(int argc, char* argv[]) {
     global_size = argo::conew_<int>(MAX_VERTICES);
     io_node_id  = argo::conew_<int>(0);
     pagerank    = argo::conew_array<double>(MAX_VERTICES);
-    test        = argo::conew_array<int>(MAX_VERTICES);
+    inlinks        = argo::conew_array<int>(MAX_VERTICES);
     exists      = argo::conew_array<bool>(MAX_VERTICES);
-    dangling    = argo::conew_array<bool>(MAX_VERTICES);
     outlinks    = argo::conew_array<int>(MAX_VERTICES);
     W_index     = argo::conew_array<int*>(MAX_VERTICES);
 
     for (int i = 0; i < MAX_VERTICES; i++)
         W_index[i] = argo::conew_array<int>(MAX_DEGREE);
 
+    argo::barrier();
+
     // Read in graph from file by hopefully only one node
-    read_graph_from_file(filename);
+    read_graph_from_file(input_filename);
 
     argo::barrier();
 
@@ -207,7 +205,7 @@ int main(int argc, char* argv[]) {
     // Initialize ArgoDSM global memory variables
     if (argo::node_id() == 0)
         for(int i = 0; i < vertices; i++)
-            pagerank[i] = INITIAL_PAGERANK;
+            pagerank[i] = 1.0/vertices; //  1.0-DAMPING_FACTOR;
 
     argo::barrier();
 
@@ -220,13 +218,10 @@ int main(int argc, char* argv[]) {
         int rem = vertices % global_num_threads;
         for (int i = 0; i < global_num_threads; i++) {
             arguments[i].global_thread_id = i;
-            if (node_chunk % local_num_threads == 0)
-                node_chunk = 0;
-            if (rem != 0 && arguments[i].global_thread_id < rem) {
+            if (rem != 0 && i < rem) {
                 arguments[i].data_begin = node_chunk;
                 arguments[i].data_end = arguments[i].data_begin + (thread_chunk+1);
                 node_chunk += (thread_chunk+1);
-                rem--;
             } else {
                 arguments[i].data_begin = node_chunk;
                 arguments[i].data_end = arguments[i].data_begin + thread_chunk;
@@ -246,20 +241,23 @@ int main(int argc, char* argv[]) {
         chunk->begin += arguments[i].data_end - arguments[i].data_begin;
 
     chunk->pagerank = std::vector<double>(chunk->size);
-    chunk->test     = std::vector<int>(chunk->size);
+    chunk->inlinks     = std::vector<int>(chunk->size);
     chunk->exists   = std::vector<bool>(chunk->size);
-    chunk->dangling = std::vector<bool>(chunk->size);
     chunk->outlinks = std::vector<int>(chunk->size);
-    chunk->W_index  = std::vector<std::vector<int>>(chunk->size);
 
     for (int j = 0; j < chunk->size; j++) {
-        chunk->test[j]     = test[chunk->begin+j];
+        chunk->inlinks[j]     = inlinks[chunk->begin+j];
+        std::cout << "inlinks[" << (chunk->begin+j) << "] = " << inlinks[chunk->begin+j] << "\n";
         chunk->exists[j]   = exists[chunk->begin+j];
-        chunk->dangling[j] = dangling[chunk->begin+j];
+        std::cout << "exists[" << (chunk->begin+j) << "] = " << exists[chunk->begin+j] << "\n";
         chunk->outlinks[j] = outlinks[chunk->begin+j];
-        chunk->W_index[j]  = std::vector<int>(MAX_DEGREE);
-        for (int i = 0; i < MAX_DEGREE; i++)
-            chunk->W_index[j][i] = W_index[chunk->begin+j][i];
+        std::cout << "outlinks[" << (chunk->begin+j) << "] = " << outlinks[chunk->begin+j] << std::endl;
+    }
+
+    //Debug
+    std::cout << "argo::number_of_nodes = " << argo::number_of_nodes() << "\nargo::node_id = " << argo::node_id() << "\nchunk->size = " << chunk->size << "\nlocal_num_threads = " << local_num_threads << std::endl;
+    for (int i = 0; i < global_num_threads; i++) {
+        std::cout << "thread[ "<< i <<" ]: " << arguments[i].data_begin << " - " << arguments[i].data_end << std::endl;
     }
 
     std::vector<pthread_t> threads(local_num_threads);
@@ -275,14 +273,13 @@ int main(int argc, char* argv[]) {
 
     argo::barrier();
 
-    write_pagerank_to_file("file.txt", pagerank, vertices);
+    write_pagerank_to_file(output_filename, pagerank, vertices);
 
     delete chunk;
     delete lock;
 
-    argo::codelete_array(test);
+    argo::codelete_array(inlinks);
     argo::codelete_array(exists);
-    argo::codelete_array(dangling);
     argo::codelete_array(outlinks);
     argo::codelete_array(W_index);
     argo::codelete_array(pagerank);
