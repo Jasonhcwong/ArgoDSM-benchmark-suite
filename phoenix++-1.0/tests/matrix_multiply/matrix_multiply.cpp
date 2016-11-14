5a6090ff8954669e8fd9ec15b8b2ea87f55a4288
farez <f4arez@gmail.com>
2016-11-14 15:12:06 +0100

    Made matrix_multiply argo compatible, yeilds same result on singlenode, mpirun on tintin and vanilla phoenix

--- phoenix++-1.0/tests/matrix_multiply/matrix_multiply.cpp
+++ phoenix++-1.0/tests/matrix_multiply/matrix_multiply.cpp
@@ -8,8 +8,8 @@
 *     * Redistributions in binary form must reproduce the above copyright
 *       notice, this list of conditions and the following disclaimer in the
 *       documentation and/or other materials provided with the distribution.
-*     * Neither the name of Stanford University nor the names of its 
-*       contributors may be used to endorse or promote products derived from 
+*     * Neither the name of Stanford University nor the names of its
+*       contributors may be used to endorse or promote products derived from
 *       this software without specific prior written permission.
 *
 * THIS SOFTWARE IS PROVIDED BY STANFORD UNIVERSITY ``AS IS'' AND ANY
@@ -22,7 +22,7 @@
 * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/ 
+*/
 
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -48,25 +48,25 @@ class MatrixMulMR : public MapReduce<MatrixMulMR, mm_data_t, int, int>
     int *output;
 
 public:
-    explicit MatrixMulMR(int* _mA, int* _mB, int size, int* out) : 
+    explicit MatrixMulMR(int* _mA, int* _mB, int size, int* out) :
         matrix_A(_mA), matrix_B(_mB), matrix_size(size), row(0), output(out) {}
-        
+
     void* locate(mm_data_t* d, uint64_t len) const
     {
         return d->matrix_A + d->row_num * d->matrix_len;
     }
 
     /** matrixmul_map()
-     * Multiplies the allocated regions of matrix to compute partial sums 
+     * Multiplies the allocated regions of matrix to compute partial sums
      */
     void map(mm_data_t const& data, map_container& out) const
     {
-        int* a_ptr = data.matrix_A + data.row_num*data.matrix_len + 0;
+        int* a_ptr = data.matrix_A + data.row_num * data.matrix_len + 0;
         int* b_ptr = data.matrix_B + 0;
-            
-        int* output = data.output + data.row_num*data.matrix_len;
-        for(int i = 0; i < data.matrix_len ; i++) {
-            for(int j=0;j<data.matrix_len ; j++) {
+
+        int* output = data.output + data.row_num * data.matrix_len;
+        for (int i = 0; i < data.matrix_len ; i++) {
+            for (int j = 0; j < data.matrix_len ; j++) {
                 output[j] += a_ptr[i] * b_ptr[j];
             }
             b_ptr += data.matrix_len;
@@ -74,7 +74,7 @@ public:
     }
 
     /** matrixmul_split()
-     *  Assign a set of rows of the output matrix to each map task 
+     *  Assign a set of rows of the output matrix to each map task
      * (for now, default to one row per task)
      */
     int split(mm_data_t& out)
@@ -91,7 +91,7 @@ public:
         out.output = output;
         out.rows = 1;
         out.row_num = row++;
-        
+
         /* Return true since the out data is valid. */
         return 1;
     }
@@ -99,19 +99,19 @@ public:
 
 int main(int argc, char *argv[]) {
 
-    int i,j, create_files;
+    int i, j, create_files;
     int fd_A, fd_B, file_size;
-    int * fdata_A, *fdata_B;
+    int * fdata_A, *fdata_B, *argo_data_A, *argo_data_B;
     int matrix_len, row_block_len;
     struct stat finfo_A, finfo_B;
     char const* fname_A, *fname_B;
     int ret;
-    
+
     struct timespec begin, end;
 
     get_time (begin);
-    argo::init(100*1024*1024UL);
-    
+    argo::init(100 * 1024 * 1024UL);
+
     srand( (unsigned)time( NULL ) );
 
     // Make sure a filename is specified
@@ -124,16 +124,16 @@ int main(int argc, char *argv[]) {
     fname_A = "matrix_file_A.txt";
     fname_B = "matrix_file_B.txt";
     CHECK_ERROR ( (matrix_len = atoi(argv[1])) < 0);
-    file_size = ((matrix_len*matrix_len))*sizeof(int);
+    file_size = ((matrix_len * matrix_len)) * sizeof(int);
 
     fprintf(stderr, "***** file size is %d\n", file_size);
 
-    if(argv[2] == NULL)
+    if (argv[2] == NULL)
         row_block_len = 1;
     else
         CHECK_ERROR ( (row_block_len = atoi(argv[2])) < 0);
 
-    if(argv[3] != NULL)
+    if (argv[3] != NULL)
         create_files = 1;
     else
         create_files = 0;
@@ -143,20 +143,20 @@ int main(int argc, char *argv[]) {
     printf("MatrixMult: Running...\n");
 
     /* If the matrix files do not exist, create them */
-    if(create_files)
+    if (create_files)
     {
         dprintf("Creating files\n");
 
         int value = 0;
-        CHECK_ERROR((fd_A = open(fname_A,O_CREAT | O_RDWR,S_IRWXU)) < 0);
-        CHECK_ERROR((fd_B = open(fname_B,O_CREAT | O_RDWR,S_IRWXU)) < 0);
-        
-        for(i=0;i<matrix_len;i++)
+        CHECK_ERROR((fd_A = open(fname_A, O_CREAT | O_RDWR, S_IRWXU)) < 0);
+        CHECK_ERROR((fd_B = open(fname_B, O_CREAT | O_RDWR, S_IRWXU)) < 0);
+
+        for (i = 0; i < matrix_len; i++)
         {
-            for(j=0;j<matrix_len;j++)
+            for (j = 0; j < matrix_len; j++)
             {
-                value = (rand())%11;
-                ret = write(fd_A,&value,sizeof(int));
+                value = (rand()) % 11;
+                ret = write(fd_A, &value, sizeof(int));
                 assert(ret == sizeof(int));
                 //dprintf("%d  ",value);
             }
@@ -164,12 +164,12 @@ int main(int argc, char *argv[]) {
         }
         //dprintf("\n");
 
-        for(i=0;i<matrix_len;i++)
+        for (i = 0; i < matrix_len; i++)
         {
-            for(j=0;j<matrix_len;j++)
+            for (j = 0; j < matrix_len; j++)
             {
-                value = (rand())%11;
-                ret = write(fd_B,&value,sizeof(int));
+                value = (rand()) % 11;
+                ret = write(fd_B, &value, sizeof(int));
                 assert(ret == sizeof(int));
                 //dprintf("%d  ",value);
             }
@@ -181,53 +181,69 @@ int main(int argc, char *argv[]) {
     }
 
     // Read in the file
-    CHECK_ERROR((fd_A = open(fname_A,O_RDONLY)) < 0);
+    CHECK_ERROR((fd_A = open(fname_A, O_RDONLY)) < 0);
     // Get the file info (for file length)
     CHECK_ERROR(fstat(fd_A, &finfo_A) < 0);
+
+#define NO_MMAP
 #ifndef NO_MMAP
 #ifdef MMAP_POPULATE
     // Memory map the file
-    CHECK_ERROR((fdata_A = (int*)mmap(0, finfo_A.st_size + 1, 
-        PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd_A, 0)) == NULL);
+    CHECK_ERROR((fdata_A = (int*)mmap(0, finfo_A.st_size + 1,
+                                      PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd_A, 0)) == NULL);
 #else
     // Memory map the file
-    CHECK_ERROR((fdata_A = (int*)mmap(0, finfo_A.st_size + 1, 
-        PROT_READ, MAP_PRIVATE, fd_A, 0)) == NULL);
+    CHECK_ERROR((fdata_A = (int*)mmap(0, finfo_A.st_size + 1,
+                                      PROT_READ, MAP_PRIVATE, fd_A, 0)) == NULL);
 #endif
 #else
-    int ret;
+//    int ret;
 
-    fdata_A = (char *)malloc (file_size);
+    fdata_A = (int *)malloc (file_size);
     CHECK_ERROR (fdata_A == NULL);
 
     ret = read (fd_A, fdata_A, file_size);
     CHECK_ERROR (ret != file_size);
+    argo_data_A = argo::conew_array<int>(finfo_A.st_size);
+    if (argo::node_id() == 0) {
+        memcpy(argo_data_A, fdata_A, finfo_A.st_size);
+    }
+    argo::barrier();
 #endif
 
     // Read in the file
-    CHECK_ERROR((fd_B = open(fname_B,O_RDONLY)) < 0);
+    CHECK_ERROR((fd_B = open(fname_B, O_RDONLY)) < 0);
     // Get the file info (for file length)
     CHECK_ERROR(fstat(fd_B, &finfo_B) < 0);
 #ifndef NO_MMAP
 #ifdef MMAP_POPULATE
     // Memory map the file
-    CHECK_ERROR((fdata_B = (int*)mmap(0, finfo_B.st_size + 1, 
-        PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd_B, 0)) == NULL);
+    CHECK_ERROR((fdata_B = (int*)mmap(0, finfo_B.st_size + 1,
+                                      PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd_B, 0)) == NULL);
 #else
     // Memory map the file
-    CHECK_ERROR((fdata_B = (int*)mmap(0, finfo_B.st_size + 1, 
-        PROT_READ, MAP_PRIVATE, fd_B, 0)) == NULL);
+    CHECK_ERROR((fdata_B = (int*)mmap(0, finfo_B.st_size + 1,
+                                      PROT_READ, MAP_PRIVATE, fd_B, 0)) == NULL);
 #endif
 #else
-    fdata_B = (char *)malloc (file_size);
+    fdata_B = (int *)malloc (file_size);
     CHECK_ERROR (fdata_B == NULL);
 
     ret = read (fd_B, fdata_B, file_size);
     CHECK_ERROR (ret != file_size);
+    argo_data_B = argo::conew_array<int>(finfo_B.st_size);
+    if (argo::node_id() == 0) {
+        memcpy(argo_data_B, fdata_B, finfo_B.st_size);
+    }
+    argo::barrier();
 #endif
 
-    int* output = (int*)calloc(matrix_len*matrix_len, sizeof(int));
-
+    int* output = (int*)calloc(matrix_len * matrix_len, sizeof(int));
+    int* argo_out = argo::conew_array<int>(matrix_len * matrix_len);
+    if (argo::node_id() == 0) {
+        memcpy(argo_out, output, matrix_len * matrix_len * sizeof(int));
+    }
+    argo::barrier();
     fprintf(stderr, "***** data size is %ld\n", (intptr_t)file_size);
     printf("MatrixMult: Calling MapReduce Scheduler Matrix Multiplication\n");
 
@@ -236,19 +252,18 @@ int main(int argc, char *argv[]) {
 
     get_time (begin);
     std::vector<MatrixMulMR::keyval> result;
-    MatrixMulMR mapReduce(fdata_A, fdata_B, matrix_len, output);
+    MatrixMulMR mapReduce(argo_data_A, argo_data_B, matrix_len, argo_out);
     mapReduce.run(result);
     get_time (end);
     print_time("library", begin, end);
-
-    get_time (begin);
-    int sum = 0;
-    for(i=0;i<matrix_len*matrix_len;i++)
-    {
-          sum += output[i];
+    if (argo::node_id() == 0) {
+        get_time (begin);
+        int sum = 0;
+        for (i = 0; i < matrix_len * matrix_len; i++) {
+            sum += argo_out[i];
+        }
+        printf ("MatrixMult: total sum is %d\n", sum);
     }
-    printf ("MatrixMult: total sum is %d\n", sum);
-
     printf("MatrixMult: MapReduce Completed\n");
 
     free(output);
