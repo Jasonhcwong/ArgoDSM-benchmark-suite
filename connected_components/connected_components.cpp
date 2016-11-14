3b945376dc03b37af03e497667fd103c3526d539
Tim Weidner <mail@timweidner.com>
2016-11-14 18:25:13 +0100

    partially localized termination condition

--- connected_components/connected_components.cpp
+++ connected_components/connected_components.cpp
@@ -3,32 +3,48 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <mutex>
 
 #include "argo.hpp"
 
-#define BILLION 1E9
-
 // Connected Components Constants
 int    MAX_VERTICES     = 2000000;
-int    MAX_DEGREE       = 16;
+int    MAX_DEGREE       = 32;
 int    MAX_COMPONENTS   = 100000000;
 
 
 struct Thread_args {
     int global_thread_id;
+    int local_thread_id;
+    int data_begin;
+    int data_end;
     int vertices;
+    pthread_barrier_t* barrier;
+};
+
+struct Chunk {
+    int begin;
+    int size;
+    std::vector<bool> exists;
+    std::vector<int> graph;
+    std::vector<int> edges;
 };
 
 
 // ArgoDSM node local variables
+int number_of_nodes;
+int node_id;
 int global_num_threads;
 int local_num_threads;
 argo::globallock::global_tas_lock *lock; // single lock
+std::mutex mutex;
+int not_changed_local;
+Chunk* chunk;
 
 
 // ArgoDMS global variables
 Thread_args* arguments;
-int*         change;
+int*         not_changed_global;
 int*         global_size;
 bool*        lock_flag;
 int*         components;
@@ -37,68 +53,70 @@ int*         edges;
 int*         graph;
 
 
-
 void* do_work(void* argptr) {
     Thread_args* args = static_cast<Thread_args*>(argptr);
 
-    int global_thread_id     = args->global_thread_id;
-    double vertices_d        = args->vertices;
+    int global_thread_id        = args->global_thread_id;
+    int local_thread_id         = args->local_thread_id;
+    int data_begin              = args->data_begin;
+    int data_end                = args->data_end;
+    pthread_barrier_t* barrier  = args->barrier;
 
-    bool mod                 = true;
-    int v                    = 0;
-    int iterations           = 0;
-    int start =  0;
-    int stop  = 0;
-    double tid_d = global_thread_id;
-    double P_d = global_num_threads;
-
-    //Chunk work for threads via double precision
-    double start_d = (tid_d) * (vertices_d/P_d);
-    double stop_d  = (tid_d+1.0) * (vertices_d/P_d);
-    start =  start_d;
-    stop  =  stop_d;
+    bool mod                    = true;
+    int iterations              = 0;
 
     argo::barrier(local_num_threads);
 
     //Each component is its own, first phase
-    for(v = start; v < stop; v++)
-        components[v] = v;
+    for(int i = data_begin; i < data_end; i++)
+        components[chunk->begin+i] = chunk->begin+i;
 
     argo::barrier(local_num_threads);
 
     //start connecting, second phase
-    while(*change < global_num_threads) {
+    while(*not_changed_global < number_of_nodes) {
         mod = false;
         iterations++;
-        for (v = start; v < stop; v++) {
-            if (exists[v]) {
-                for (int i = 0; i < edges[v]; i++) {
-                    int neighbor = graph[v*MAX_DEGREE+i];
-                    if((components[v] < components[neighbor]) && (components[neighbor] == components[components[neighbor]])) {
+        for (int j = data_begin; j < data_end; j++) {
+            if (chunk->exists[j]) {
+                for (int i = 0; i < chunk->edges[j]; i++) {
+                    int neighbor = chunk->graph[j*MAX_DEGREE+i];
+                    if((components[chunk->begin+j] < components[neighbor]) && (components[neighbor] == components[components[neighbor]])) {
+                        components[components[neighbor]] = components[chunk->begin+j];
                         mod = true;
-                        components[components[neighbor]] = components[v];
                     }
                 }
             }
         }
 
+        if (local_thread_id == 0)
+            not_changed_local = 0;
+
         if (global_thread_id == 0)
-            *change = 0;
+            *not_changed_global = 0;
 
         argo::barrier(local_num_threads);
 
-        //Third phase, assign components
-        for(v = start; v < stop; v++) {
-            while(components[v] != components[components[v]]) {
-                components[v] = components[components[v]];
-            }
+        if (!mod) {
+            mutex.lock();
+            not_changed_local++;
+            mutex.unlock();
         }
 
+        pthread_barrier_wait(barrier);
+
         //For termination Condition
-        if (!mod) {
+        if (not_changed_local == local_num_threads) {
             lock->lock();
-            (*change)++;
+            (*not_changed_global)++;
             lock->unlock();
+        }
+
+        //Third phase, assign components
+        for(int i = data_begin; i < data_end; i++) {
+            while(components[chunk->begin+i] != components[components[chunk->begin+i]]) {
+                components[chunk->begin+i] = components[components[chunk->begin+i]];
+            }
         }
 
         argo::barrier(local_num_threads);
@@ -183,7 +201,10 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+    node_id = argo::node_id();
+    number_of_nodes = argo::number_of_nodes();
     global_num_threads = atoi(argv[1]);
+    local_num_threads = global_num_threads / number_of_nodes;
     std::string input_filename = argv[2];
     std::string output_filename = argv[3];
 
@@ -194,19 +215,19 @@ int main(int argc, char** argv) {
     }
 
     Thread_args*  arguments = argo::conew_array<Thread_args>(global_num_threads);
-    global_size  = argo::conew_<int>(0);
-    change       = argo::conew_<int>(0);
-    lock_flag    = argo::conew_<bool>(false);
-    components   = argo::conew_array<int>(MAX_VERTICES);
-    exists       = argo::conew_array<bool>(MAX_VERTICES);
-    edges        = argo::conew_array<int>(MAX_VERTICES);
-    graph        = argo::conew_array<int>((MAX_VERTICES)*(MAX_DEGREE));
+    global_size         = argo::conew_<int>(0);
+    not_changed_global  = argo::conew_<int>(0);
+    lock_flag           = argo::conew_<bool>(false);
+    components          = argo::conew_array<int>(MAX_VERTICES);
+    exists              = argo::conew_array<bool>(MAX_VERTICES);
+    edges               = argo::conew_array<int>(MAX_VERTICES);
+    graph               = argo::conew_array<int>((MAX_VERTICES)*(MAX_DEGREE));
 
     argo::barrier();
 
     lock  = new argo::globallock::global_tas_lock(lock_flag);
 
-    if (argo::node_id() == 0)
+    if (node_id == 0)
         read_graph_from_file(input_filename);
 
     argo::barrier();
@@ -215,23 +236,64 @@ int main(int argc, char** argv) {
 
 
     // Initialize ArgoDSM global variables
-    if (argo::node_id() == 0)
+    if (node_id == 0)
         for(int j = 0; j < global_num_threads; j++)
             arguments[j].global_thread_id = j;
 
     argo::barrier();
 
     // Divide the work as equal as possible among global_num_threads
-    local_num_threads = global_num_threads / argo::number_of_nodes();
+    if (node_id == 0) {
+        int node_chunk = 0;
+        int thread_chunk = vertices / global_num_threads;
+        int rem = vertices % global_num_threads;
+        for (int i = 0; i < global_num_threads; i++) {
+            if (i % local_num_threads == 0) node_chunk = 0;
+            arguments[i].global_thread_id = i;
+            if (rem != 0 && i < rem) {
+                arguments[i].data_begin = node_chunk;
+                arguments[i].data_end = arguments[i].data_begin + (thread_chunk+1);
+                node_chunk += (thread_chunk+1);
+            } else {
+                arguments[i].data_begin = node_chunk;
+                arguments[i].data_end = arguments[i].data_begin + thread_chunk;
+                node_chunk += thread_chunk;
+            }
+        }
+    }
 
-    int node_threads_begin = argo::node_id() * local_num_threads;
+    argo::barrier();
+
+    // Copy relevant graph chunk to local memory of the argo node
+    chunk = new Chunk();
+    int node_threads_begin = node_id * local_num_threads;
+    chunk->size = arguments[node_threads_begin + local_num_threads - 1].data_end;
+    chunk->begin = 0;
+    for (int i = 0; i < node_threads_begin; i++)
+        chunk->begin += arguments[i].data_end - arguments[i].data_begin;
+
+    chunk->exists = std::vector<bool>(chunk->size);
+    chunk->edges  = std::vector<int>(chunk->size);
+    chunk->graph  = std::vector<int>(chunk->size*MAX_DEGREE);
+
+    for (int j = 0; j < chunk->size; j++) {
+        chunk->exists[j] = exists[chunk->begin+j];
+        chunk->edges[j]  = edges[chunk->begin+j];
+        for (int i = 0; i < MAX_DEGREE; i++)
+            chunk->graph[j*MAX_DEGREE+i] = graph[(chunk->begin+j)*MAX_DEGREE+i];
+    }
+
     std::vector<pthread_t> threads(local_num_threads);
+    pthread_barrier_t barrier;
+    pthread_barrier_init(&barrier, NULL, local_num_threads);
 
     auto start = std::chrono::system_clock::now();
 
     for (int i = 0; i < local_num_threads; i++) {
         int j = node_threads_begin + i;
+        arguments[j].local_thread_id = i;
         arguments[j].vertices = vertices;
+        arguments[j].barrier = &barrier;
         pthread_create(&threads[i], nullptr, do_work, &arguments[j]);
     }
 
@@ -243,10 +305,10 @@ int main(int argc, char** argv) {
 
     argo::barrier();
 
-    if (argo::node_id() == 0) {
+    if (node_id == 0) {
         write_connected_components_to_file(output_filename, components, vertices);
         std::cout << "\nConnected components\n";
-        std::cout << "Argo nodes: " << argo::number_of_nodes() << "\nGlobal threads: " << global_num_threads << "\nLocal threads: " << local_num_threads << "\nGraph: " << input_filename << "\n";
+        std::cout << "Argo nodes: " << number_of_nodes << "\nGlobal threads: " << global_num_threads << "\nLocal threads: " << local_num_threads << "\nGraph: " << input_filename << "\n";
         std::cout << "Runtime: " << elapsed.count() << " ms\n" << std::endl;
     }
 
@@ -254,7 +316,7 @@ int main(int argc, char** argv) {
 
     argo::codelete_array(arguments);
     argo::codelete_(global_size);
-    argo::codelete_(change);
+    argo::codelete_(not_changed_global);
     argo::codelete_(lock_flag);
     argo::codelete_array(components);
     argo::codelete_array(exists);
