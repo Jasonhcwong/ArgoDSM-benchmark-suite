2f6b6875b3f7dcacae9341c078cbbd0bb8d96a9d
Tim Weidner <mail@timweidner.com>
2016-11-23 14:13:04 +0100

    cleanup

--- scripts/compile_argo.sh
+++ scripts/compile_argo.sh
@@ -1,15 +1,15 @@
-cd argodsm
-cd tests
+cd ../argodsm/tests
 wget https://github.com/google/googletest/releases/tag/release-1.7.0.zip
 unzip release-1.7.0.zip
 mv googletest-release-1.7.0 gtest-1.7.0
-cd ..
-mkdir build
-cd build
+mkdir ../build
+cd ../build
 cmake -DARGO_BACKEND_MPI=ON        \
       -DARGO_BACKEND_SINGLENODE=ON \
       -DARGO_TESTS=ON              \
       -DBUILD_DOCUMENTATION=ON     \
+      -DARGO_VM_SHM=OFF            \
+      -DARGO_VM_MEMFD=ON           \
       -DCMAKE_CXX_COMPILER=mpic++  \
       -DCMAKE_C_COMPILER=mpicc     \
       ../
--- scripts/job.sh
+++ scripts/job.sh
@@ -6,13 +6,10 @@ vertices=$3
 threads=$(expr ${argonodes} \* 8)
 
 echo "#!/bin/bash -l
-#SBATCH -A g2016027
-#SBATCH -p node -N ${nodes} -n ${threads} 
+#SBATCH -p cluster -N ${nodes} -n ${threads} 
 #SBATCH -t 5:00
 #SBATCH -J ${algorithm}_${vertices} 
-module load gcc/5.2.0
-module load openmpi/1.8.8
-cd /proj/g2016027/tim/project/${algorithm}
+cd ../${algorithm}
 mpirun -v --map-by ppr:2:node --mca mpi_leave_pinned 1 --mca btl openib,self,sm -n ${argonodes} ./${algorithm} ${threads} testfiles/${algorithm}_${vertices}.txt output/${algorithm}_${vertices}.txt"
 
 
